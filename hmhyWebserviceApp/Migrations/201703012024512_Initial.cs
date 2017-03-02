f358f0241e4c4ad6028fbade04c8aabff39c6457
Kody <agent.xaber@gmail.com>
2017-03-01 22:11:59 -0700

    Added connection string to database

---
+++ hmhyWebserviceApp/Migrations/201703012024512_Initial.cs
@@ -0,0 +1,28 @@
+namespace hmhyWebserviceApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        userId = c.Int(nullable: false, identity: true),
+                        userCreds_userId = c.Int(nullable: false),
+                        userCreds_userName = c.String(),
+                        userCreds_password = c.String(),
+                    })
+                .PrimaryKey(t => t.userId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
